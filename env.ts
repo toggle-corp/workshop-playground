b4297049d3f1011b8c7d03dc6980ef3068ab72ef
subinasr <ssr.subina@gmail.com>
2025-07-16 17:31:52 +0545

    chore(web-app-serve): Add configuration for web-app-serve

--- env.ts
+++ env.ts
@@ -1,23 +1,37 @@
-import { defineConfig, Schema } from '@julr/vite-plugin-validate-env';
+import {
+    defineConfig,
+    overrideDefineForWebAppServe,
+    Schema,
+} from '@julr/vite-plugin-validate-env';
+
+const webAppServeEnabled = process.env.WEB_APP_SERVE_ENABLED?.toLowerCase() === 'true';
+if (webAppServeEnabled) {
+    // eslint-disable-next-line no-console
+    console.warn('Building application for web-app-serve');
+}
+const overrideDefine = webAppServeEnabled
+    ? overrideDefineForWebAppServe
+    : undefined;
 
 export default defineConfig({
-    APP_TITLE: Schema.string(),
-    APP_ENVIRONMENT: (key, value) => {
-        // NOTE: APP_ENVIRONMENT_PLACEHOLDER is meant to be used with image builds
-        // The value will be later replaced with the actual value
-        const regex = /^production|staging|testing|alpha-\d+|development|APP_ENVIRONMENT_PLACEHOLDER$/;
-        const valid = !!value && (value.match(regex) !== null);
-        if (!valid) {
-            throw new Error(`Value for environment variable "${key}" must match regex "${regex}", instead received "${value}"`);
-        }
-        if (value === 'APP_ENVIRONMENT_PLACEHOLDER') {
-            console.warn(`Using ${value} for app environment. Make sure to not use this for builds without nginx-serve`)
-        }
-        return value as ('production' | 'staging' | 'testing' | `alpha-${number}` | 'development' | 'APP_ENVIRONMENT_PLACEHOLDER');
+    overrideDefine,
+    validator: 'builtin',
+    schema: {
+        APP_TITLE: Schema.string(),
+        APP_ENVIRONMENT: (key, value) => {
+            // NOTE: APP_ENVIRONMENT_PLACEHOLDER is meant to be used with image builds
+            // The value will be later replaced with the actual value
+            const regex = /^production|staging|testing|alpha-\d+|development|APP_ENVIRONMENT_PLACEHOLDER$/;
+            const valid = !!value && (value.match(regex) !== null);
+            if (!valid) {
+                throw new Error(`Value for environment variable "${key}" must match regex "${regex}", instead received "${value}"`);
+            }
+            if (value === 'APP_ENVIRONMENT_PLACEHOLDER') {
+                console.warn(`Using ${value} for app environment. Make sure to not use this for builds without nginx-serve`);
+            }
+            return value as ('production' | 'staging' | 'testing' | `alpha-${number}` | 'development' | 'APP_ENVIRONMENT_PLACEHOLDER');
+        },
+        APP_GRAPHQL_DOMAIN: Schema.string(),
+        APP_SENTRY_DSN: Schema.string.optional(),
     },
-    APP_GRAPHQL_CODEGEN_ENDPOINT: Schema.string(),
-    APP_GRAPHQL_DOMAIN: Schema.string(),
-    APP_UMAMI_SRC: Schema.string.optional(),
-    APP_UMAMI_ID: Schema.string.optional(),
-    APP_SENTRY_DSN: Schema.string.optional(),
 });

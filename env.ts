67f5d94c50e331ee1b7455fea703f2b9970488c7
thenav56 <ayernavin@gmail.com>
2025-07-17 14:37:14 +0545

    fix: type issue in env.ts

--- env.ts
+++ env.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     validator: 'builtin',
     schema: {
         APP_TITLE: Schema.string(),
-        APP_ENVIRONMENT: (key, value) => {
+        APP_ENVIRONMENT: (key: string, value: string) => {
             // NOTE: APP_ENVIRONMENT_PLACEHOLDER is meant to be used with image builds
             // The value will be later replaced with the actual value
             const regex = /^production|staging|testing|alpha-\d+|development|APP_ENVIRONMENT_PLACEHOLDER$/;
@@ -28,7 +28,8 @@ export default defineConfig({
                 throw new Error(`Value for environment variable "${key}" must match regex "${regex}", instead received "${value}"`);
             }
             if (value === 'APP_ENVIRONMENT_PLACEHOLDER') {
-                console.warn(`Using ${value} for app environment. Make sure to not use this for builds without nginx-serve`)
+                // eslint-disable-next-line no-console
+                console.warn(`Using ${value} for app environment. Make sure to not use this for builds without nginx-serve`);
             }
             return value as ('production' | 'staging' | 'testing' | `alpha-${number}` | 'development' | 'APP_ENVIRONMENT_PLACEHOLDER');
         },
